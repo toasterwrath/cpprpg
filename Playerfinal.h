e82eee6f709c9b755649367f27816eedf9e7e78f
toasterwrath <draconwaffle@gmail.com>
2013-01-15 13:00:48 -0800

    Create Playerfinal.h

---
+++ Playerfinal.h
@@ -0,0 +1,16 @@
+class Player 
+{
+public:
+  int addXP = goblinHP - hp;
+	int addGold = goblinHP - ap;
+	int strength;
+	int ap;
+	int hp;
+	int playerLevel;
+	int playerXP;
+	int mana;
+	int playerClass;
+	int playerChoice;
+	int playerBattle;
+	int playerGold = 0 + addGold;
+}
